9ae1888e6f859ffc8d6fa655341edb32d3be09c1
Rishika Kumari <rishika09090@gmail.com>
2025-02-09 22:48:39 +0530

    Complete Responsive Navbar

--- Rishika_Portfolio/src/components/navbar/NavbarLogo.jsx
+++ Rishika_Portfolio/src/components/navbar/NavbarLogo.jsx
@@ -3,7 +3,10 @@ import React from "react";
 const NavbarLogo = () => {
   return (
     <div>
-      <h1 className="text-white text-2xl">Rishika Kumari</h1>
+      <h1 className="text-white text-2xl sm:hidden md:block">Rishika Kumari</h1>
+      <h1 className="text-white font-special font-extrabold text-4xl sm:block md:hidden">
+        RK
+      </h1>
     </div>
   );
 };
