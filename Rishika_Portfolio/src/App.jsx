95ae6dcd5d4b26db0051748b514c8585b8ba109c
Rishika Kumari <rishika09090@gmail.com>
2025-02-09 21:25:01 +0530

    Project Setup and Navbar Setup

---
+++ Rishika_Portfolio/src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import NavBarMain from "./components/navbar/NavBarMain";
+
+function App() {
+  return (
+    <main className="font-body">
+      <NavBarMain />
+    </main>
+  );
+}
+
+export default App;
